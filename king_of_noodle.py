094a47cc9c590a4e0714c25014f968751dec5396
susanctg <susanctg@gmail.com>
2017-10-06 16:57:19 +0700

    change bg and png pic

--- king_of_noodle.py
+++ king_of_noodle.py
@@ -11,24 +11,21 @@ class SpaceGameWindow(arcade.Window):
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.WHITE)
-        self.bg = arcade.Sprite('image/kitchen.png')
+        self.bg = arcade.Sprite('image/full.jpg')
         self.bg.set_position(width//2,height//2)
-        self.title = arcade.Sprite('image/kon_6.png')
-        self.title.set_position(width-400,height-85)
-        self.bowl = arcade.Sprite('image/bowl.png',0.65)
-        self.bowl.set_position(width-400,250)
-        self.noodleS = arcade.Sprite('image/noodles2.png',0.65)
-        self.noodleS.set_position(width-400,250)
-        self.waterS = arcade.Sprite('image/water.png',0.65)
-        self.waterS.set_position(width-400,250)
-        self.boilS = arcade.Sprite('image/boil.png',0.65)
-        self.boilS.set_position(width-400,250)
+        self.bowl = arcade.Sprite('image/bowl.png',0.8)
+        self.bowl.set_position(width-400,200)
+        self.noodleS = arcade.Sprite('image/noodles.png',0.8)
+        self.noodleS.set_position(width-400,200)
+        self.waterS = arcade.Sprite('image/water.png',0.8)
+        self.waterS.set_position(width-400,200)
+        self.boilS = arcade.Sprite('image/boil.png',0.8)
+        self.boilS.set_position(width-400,200)
         
         
     def on_draw(self):
         arcade.start_render()
         self.bg.draw()
-        self.title.draw()
         self.bowl.draw()
         if(self.ndworld.outkey == 'n'):
             self.noodleS.draw()
