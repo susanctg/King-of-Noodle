14773bb680933c36b1a98943da20b1e31bd35b98
susanctg <susanctg@gmail.com>
2017-10-06 13:17:23 +0700

    add key noodle and water

--- king_of_noodle.py
+++ king_of_noodle.py
@@ -1,25 +1,39 @@
 import arcade
- 
+import arcade.key
+from noodle import Noodle,World
+
 SCREEN_WIDTH = 1191
 SCREEN_HEIGHT = 670
 
 class SpaceGameWindow(arcade.Window):
-
-    def on_draw(self):
-        arcade.start_render()
-        self.bg.draw()
-        self.title.draw()
-
     def __init__(self, width, height):
+        self.ndworld = World(width,height)
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.WHITE)
-
         self.bg = arcade.Sprite('image/kitchen.png')
         self.bg.set_position(width//2,height//2)
         self.title = arcade.Sprite('image/kon_6.png')
         self.title.set_position(width-400,height-85)
-
+        self.bowl = arcade.Sprite('image/bowl.png')
+        self.bowl.set_position(width-400,250)
+        self.noodleS = arcade.Sprite('image/noodles2.png')
+        self.noodleS.set_position(width-400,250)
+        self.waterS = arcade.Sprite('image/water.png')
+        self.waterS.set_position(width-400,250)
+        
+    def on_draw(self):
+        arcade.start_render()
+        self.bg.draw()
+        self.title.draw()
+        self.bowl.draw()
+        if(self.ndworld.outkey == 'n'):
+            self.noodleS.draw()
+        if(self.ndworld.outkey == 'w'):
+            self.waterS.draw()
+        
+    def on_key_press(self, key, key_modifiers):
+        self.ndworld.on_key_press(key, key_modifiers)
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
